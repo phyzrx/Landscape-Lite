1e1b5684be39ed88a638ecbb4394788cd3bf73ba
phyzrx <33519045+phyzrx@users.noreply.github.com>
2024-05-18 02:59:31 -0700

    Add files via upload

---
+++ Python Session Handler.py
@@ -0,0 +1,19 @@
+import os
+import signal
+
+def Call():
+    returnstr = "Python Session Handler Called"
+    print(returnstr)
+    return 0
+
+def GetPID():
+    pid = os.getpid()
+    returnstr = "Session PID %g" % (pid)
+    print(returnstr)
+    return pid
+
+def Stop(pid):
+    os.kill(pid, signal.SIGTERM)
+    returnstr = "Killed Session PID %g" % (pid)
+    print(returnstr)
+    return 0
